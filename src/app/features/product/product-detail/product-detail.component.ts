c47f91e955e94900a65e77b5caa89ec125d237d4
mohamedfofana <manoufof@hotmail.com>
2025-01-26 07:11:13 -0500

    add loader

--- src/app/features/product/product-detail/product-detail.component.ts
+++ src/app/features/product/product-detail/product-detail.component.ts
@@ -3,14 +3,10 @@ import {
   Component,
   inject,
   signal,
-  Signal,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { ProductService } from '../../../core/services/http/product/product.service';
-import { Product } from '../../../core/model/db/product';
-import { UtilsService } from '../../../core/services/utils/utils/utils.service';
-import { EMPTY, map, switchMap } from 'rxjs';
+import { map, switchMap } from 'rxjs';
 import { ProductDescriptionComponent } from '../../../shared/components/product/details/tabs/product-tab-description/product-tab-description.component';
 import { ProductDetailsImagesComponent } from '../../../shared/components/product/details/product-details-images/product-details-images.component';
 import { ProductDetailsInfoComponent } from '../../../shared/components/product/details/product-details-info/product-details-info.component';
@@ -18,8 +14,8 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { ProductTabReviewsComponent } from '../../../shared/components/product/details/tabs/product-tab-reviews/product-tab-reviews.component';
 import { ProductDetailsActionComponent } from '../../../shared/components/product/product-details-action/product-details-action.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { NoDataFoundComponent } from '../../no-data-found/no-data-found.component';
 import { AsyncPipe, NgIf } from '@angular/common';
+import { LoaderComponent } from '../../../shared/components/common/loader/loader.component';
 
 @Component({
   selector: 'app-product-details',
@@ -30,11 +26,11 @@ import { AsyncPipe, NgIf } from '@angular/common';
     ProductDescriptionComponent,
     ProductDetailsImagesComponent,
     ProductDetailsInfoComponent,
-    NoDataFoundComponent,
     ProductDetailsActionComponent,
     NgIf,
     AsyncPipe,
     TranslateModule,
+    LoaderComponent,
   ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
@@ -46,5 +42,4 @@ export class ProductDetailsComponent {
   reviewsCount = signal<number>(0);
   id$ = this.activatedRoute.params.pipe(map((params) => params['id']));
   product$ = this.id$.pipe(switchMap((id) => this.productService.find(id)));
-
 }
